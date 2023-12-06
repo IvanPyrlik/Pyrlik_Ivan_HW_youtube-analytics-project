920e355dfce71f7443a345ef77d29812d4869d05
IvanPyrlik <ivan.pyrlik59@gmail.com>
2023-12-06 20:33:39 +0500

    correction channel.py

--- src/channel.py
+++ src/channel.py
@@ -16,11 +16,13 @@ class Channel:
 
         api_key: str = os.getenv('API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
-
-        def printj(dict_to_print: dict) -> None:
-            """Выводит словарь в json-подобном удобном формате с отступами"""
-            print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-        # channel_id = 'AIzaSyCvIQes6xj93ZEizTMz29Scrt7K8We1Y10'  # HighLoad Channel
+        # channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        channel_id = 'AIzaSyCvIQes6xj93ZEizTMz29Scrt7K8We1Y10'  # HighLoad Channel
         channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         return printj(channel)
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
