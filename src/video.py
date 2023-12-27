269d522f98490312d135431446d2b4061701882f
IvanPyrlik <ivan.pyrlik59@gmail.com>
2023-12-27 10:28:21 +0500

    exceptions video.py

--- src/video.py
+++ src/video.py
@@ -10,13 +10,19 @@ class Video:
         self.__video_id = video_id
         self.video = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                       id=video_id).execute()
-        self.video_title = self.video['items'][0]['snippet']['title']
-        self.video_url = f'https://youtu.be/{self.__video_id}'
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{self.__video_id}'
+            self.view_count = self.video['items'][0]['statistics']['viewCount']
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
     @classmethod
     def get_service(cls):
